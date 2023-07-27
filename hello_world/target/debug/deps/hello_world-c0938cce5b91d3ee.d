4337a87c9b4e556bd991afaaad7f05c820b040f5
Débora Marim <deboramarim77@hotmail.com>
2023-07-26 22:28:44 -0300

    Said hallo to my worldgit add .!

---
+++ hello_world/target/debug/deps/hello_world-c0938cce5b91d3ee.d
@@ -0,0 +1,5 @@
+/home/debdev/Documentos/www/RUST/Studying_Rust/hello_world/target/debug/deps/hello_world-c0938cce5b91d3ee: src/main.rs
+
+/home/debdev/Documentos/www/RUST/Studying_Rust/hello_world/target/debug/deps/hello_world-c0938cce5b91d3ee.d: src/main.rs
+
+src/main.rs:
