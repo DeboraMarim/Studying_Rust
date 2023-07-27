7a0204562882e1a04352b94e937276c87789c619
Débora Marim <deboramarim77@hotmail.com>
2023-07-26 22:37:41 -0300

    Estrutura

---
+++ hello_world/initial/src/main.rs
@@ -0,0 +1,24 @@
+// Sintaxe Básica
+
+// Funções: Em Rust, você define funções usando a palavra-chave fn. 
+// Por exemplo, a função main é o ponto de entrada do seu programa:
+
+fn main() {
+    println!("Hello, World!");
+}
+
+// Blocos de código: 
+// Em Rust, você usa chaves {} para definir blocos de código. 
+// As variáveis em Rust têm um conceito chamado "escopo", que é determinado pelo bloco em que estão.
+
+// Instruções e Expressões: 
+// Em Rust, quase tudo é uma expressão. 
+// Expressões retornam um valor e podem ser parte de instruções. 
+// Instruções não retornam um valor e são seguidas por um ponto-e-vírgula.
+
+// Variáveis e Tipos de Dados
+
+// Variáveis Imutáveis: Por padrão, as variáveis em Rust são imutáveis. 
+// Isso significa que uma vez que um valor é vinculado a um nome, você não pode mudar esse valor. 
+// Por exemplo:
+
