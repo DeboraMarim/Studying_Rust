956a1bafe8d98d13f27601b9fe53ad22abe6fb62
Débora Marim <deboramarim77@hotmail.com>
2023-07-27 08:47:07 -0300

    Data Types

---
+++ hello_world/initial/datatypes/src/main.rs
@@ -0,0 +1,36 @@
+//Tipos Escalares
+
+//Integers:
+
+let a: i32 = 5;
+let b: u64 = 100;
+
+
+//Floating-Point Numbers:
+let c: f32 = 2.0;
+let d: f64 = 3.1415;
+
+
+//Boolean:
+let e: bool = true;
+let f: bool = false;
+
+//Characters:
+let g: char = 'z';
+let h: char = '😻';
+
+
+//2. Tipos Compostos
+
+//Tuple:
+let tup1: (i32, f64, u8) = (500, 6.4, 1);
+let tup2: (char, bool, i64) = ('a', true, 100);
+
+
+//Array:
+let arr1: [i32; 5] = [1, 2, 3, 4, 5];
+let arr2: [f64; 3] = [1.1, 2.2, 3.3];
+
+//not exprecificado
+
+let a = 5;
